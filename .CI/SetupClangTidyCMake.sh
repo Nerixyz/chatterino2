849e11f23d36166f16c74c600b9a524b3f8285d2
Nerixyz <nerixdev@outlook.de>
2024-03-27 16:34:25 +0100

    fix: for real

--- .CI/SetupClangTidyCMake.sh
+++ .CI/SetupClangTidyCMake.sh
@@ -2,21 +2,15 @@
 
 set -ev;
 
-# pip3 install -U pip;
-# pip3 install aqtinstall;
-# aqt install-qt linux desktop $1 gcc_64 -m qtimageformats qt5compat --outputdir .qtinstall;
-
 # aqt installs into .qtinstall/Qt/<version>/gcc_64
 # This is doing the same as jurplel/install-qt-action
 # See https://github.com/jurplel/install-qt-action/blob/74ca8cd6681420fc8894aed264644c7a76d7c8cb/action/src/main.ts#L52-L74
 qtpath=$(echo .qtinstall/Qt/[0-9]*/*/bin/qmake | sed -e s:/bin/qmake$::);
-echo $qtpath;
 export LD_LIBRARY_PATH="$qtpath/lib";
 export QT_ROOT_DIR=$qtpath;
 export QT_PLUGIN_PATH="$qtpath/plugins";
 export PATH="$PATH:$(realpath "$qtpath/bin")";
 export Qt6_DIR="$(realpath "$qtpath")";
-echo $Qt6_DIR;
 
 cmake -S. -Bbuild-clang-tidy \
     -DCMAKE_BUILD_TYPE=Debug \
@@ -27,8 +21,7 @@ cmake -S. -Bbuild-clang-tidy \
     -DCHATTERINO_PLUGINS=On \
     -DBUILD_WITH_QT6=On \
     -DBUILD_TESTS=On \
-    -DBUILD_BENCHMARKS=On \
-    -DCMAKE_PREFIX_PATH="$(realpath "$qtpath")";
+    -DBUILD_BENCHMARKS=On;
 
 # Run MOC and UIC
 # This will compile the version project (1 file)
