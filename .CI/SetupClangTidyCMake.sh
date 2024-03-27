a375976e99b56daa20aaf20e0c7c566714c0ea24
Nerixyz <nerixdev@outlook.de>
2024-03-27 15:25:51 +0100

    fix: directory

--- .CI/SetupClangTidyCMake.sh
+++ .CI/SetupClangTidyCMake.sh
@@ -6,13 +6,13 @@ set -ev;
 # pip3 install aqtinstall;
 # aqt install-qt linux desktop $1 gcc_64 -m qtimageformats qt5compat --outputdir .qtinstall;
 
-# # aqt installs into .qtinstall/<version>/gcc_64
-# # This is doing the same as jurplel/install-qt-action
-# # See https://github.com/jurplel/install-qt-action/blob/74ca8cd6681420fc8894aed264644c7a76d7c8cb/action/src/main.ts#L52-L74
-# qtpath=${$(echo .qtinstall/[0-9]*/*/bin/qmake*)%/bin/qmake*};
-# export LD_LIBRARY_PATH="$qtpath/lib";
-# export QT_ROOT_DIR=$qtpath;
-# export QT_PLUGIN_PATH="$qtpath/plugins";
+# aqt installs into .qtinstall/Qt/<version>/gcc_64
+# This is doing the same as jurplel/install-qt-action
+# See https://github.com/jurplel/install-qt-action/blob/74ca8cd6681420fc8894aed264644c7a76d7c8cb/action/src/main.ts#L52-L74
+qtpath=${$(echo .qtinstall/Qt/[0-9]*/*/bin/qmake*)%/bin/qmake*};
+export LD_LIBRARY_PATH="$qtpath/lib";
+export QT_ROOT_DIR=$qtpath;
+export QT_PLUGIN_PATH="$qtpath/plugins";
 
 cmake -S. -Bbuild-clang-tidy \
     -DCMAKE_BUILD_TYPE=Debug \
