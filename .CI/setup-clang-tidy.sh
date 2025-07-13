3ade01793a344d6feb28083518a66902b13173e8
Nerixyz <nerixdev@outlook.de>
2025-07-13 11:12:57 +0200

    aaa

--- .CI/setup-clang-tidy.sh
+++ .CI/setup-clang-tidy.sh
@@ -25,7 +25,7 @@ cmake -S. -Bbuild-clang-tidy \
 curl -sSLo build-clang-tidy/clazy19.zip \
     https://github.com/Nerixyz/clazy-ubuntu-builds/releases/download/clazy-4764263/clazy19.zip
 unzip build-clang-tidy/clazy19.zip
-ldd -r ClazyPlugin.so | grep "not found"
+ldd -r ClazyPlugin.so
 
 # Run MOC and UIC
 # This will compile the dependencies
