05071e20bf45fef470312784ab598d7a2cd6b509
Nerixyz <nerixdev@outlook.de>
2026-06-12 15:43:35 +0200

    fix(updates): Treat 7.x as 2.x

--- src/singletons/Updates.cpp
+++ src/singletons/Updates.cpp
@@ -104,12 +104,9 @@ bool Updates::isDowngradeOf(const QString &online, const QString &current)
         return false;
     }
 
-    // TODO: remove once chatterino7's major version switches from `7` to `2`
-    if (currentVersion.major == 7 && onlineVersion.major == 2)
+    if (onlineVersion.major == 7)
     {
-        currentVersion = {2, currentVersion.minor, currentVersion.patch,
-                          currentVersion.prerelease_type,
-                          currentVersion.prerelease_number};
+        onlineVersion.major = 2;
     }
 
     return onlineVersion < currentVersion;
