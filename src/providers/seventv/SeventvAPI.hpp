a21d4c918729284819fa861b9bf9fe34aaf61601
Nerixyz <nerixdev@outlook.de>
2026-08-02 21:57:01 +0200

    perf: reduce startup lag by reusing 7TV user response

--- src/providers/seventv/SeventvAPI.hpp
+++ src/providers/seventv/SeventvAPI.hpp
@@ -29,15 +29,18 @@ public:
     SeventvAPI &operator=(const SeventvAPI &) = delete;
     SeventvAPI &operator=(SeventvAPI &&) = delete;
 
-    void getUserByTwitchID(const QString &twitchID,
-                           SuccessCallback<const QJsonObject &> &&onSuccess,
-                           ErrorCallback &&onError);
-    void getUserByKickID(uint64_t userID,
-                         SuccessCallback<const QJsonObject &> &&onSuccess,
-                         ErrorCallback &&onError);
-    void getEmoteSet(const QString &emoteSet,
-                     SuccessCallback<const QJsonObject &> &&onSuccess,
-                     ErrorCallback &&onError);
+    void getUserByTwitchID(
+        const QString &twitchID,
+        SuccessCallback<const QJsonObject &, const QByteArray &> &&onSuccess,
+        ErrorCallback &&onError);
+    void getUserByKickID(
+        uint64_t userID,
+        SuccessCallback<const QJsonObject &, const QByteArray &> &&onSuccess,
+        ErrorCallback &&onError);
+    void getEmoteSet(
+        const QString &emoteSet,
+        SuccessCallback<const QJsonObject &, const QByteArray &> &&onSuccess,
+        ErrorCallback &&onError);
 
     void updateTwitchPresence(const QString &twitchChannelID,
                               const QString &seventvUserID,
